b0c38aa5a1069878d279124fe61910fb48407d0d
Artem Melikhov <melcomeart@gmail.com>
2024-06-29 09:55:48 +0300

    commit 3 --add print+int

--- Education_AT/src/Main.java
+++ Education_AT/src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+
+        int x = 3;
+        System.out.println("Hello world!" + x);
     }
 }
