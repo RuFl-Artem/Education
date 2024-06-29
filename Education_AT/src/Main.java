8ceab96f25c4e9f184e8714852c345ccd263591a
Artem Melikhov <melcomeart@gmail.com>
2024-06-29 10:48:37 +0300

    add test print + if

--- Education_AT/src/Main.java
+++ Education_AT/src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
         //для коммита 3
         int x = 3;
-        System.out.println("Hello world!" + x);
+        System.out.println("Hello world! " + x);
         //из курсового
         System.out.println("Случайное число от 0 до 1: " + Math.random());
+        //test print + if
+        for (int i = 1; i <= 4; i++) {
+            if (i == 4) {
+                System.out.print("Hello world is if = ");
+                System.out.println(i);
+            }
+        }
     }
 }
