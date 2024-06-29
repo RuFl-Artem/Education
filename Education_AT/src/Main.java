dcfab1fab241e797a462df6d12a424e214a212bf
Artem Melikhov <melcomeart@gmail.com>
2024-06-29 10:31:32 +0300

    add ignore

--- Education_AT/src/Main.java
+++ Education_AT/src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-
+        //для коммита 3
         int x = 3;
         System.out.println("Hello world!" + x);
+//из курсового
+        System.out.println("Случайное число от 0 до 1: " + Math.random())
     }
 }
